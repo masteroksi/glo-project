679b9a1742532b9d6c10e5d8662e49934da1f5b8
masteroksi <masteroksi@gmail.com>
2020-06-08 18:26:05 +0300

    lesson01

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const money = 'money',
+    income = 'income',
+    addExpenses = 'addExpenses',
+    deposit = 'deposit',
+    mission = 'mission',
+    period = 'period';
+
+alert('Любой текст');
+
+console.log('Текст в консоле');
