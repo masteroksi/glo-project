24d53859940d93a3d1e8c0f4fc7dc9bda64b8ac2
masteroksi <masteroksi@gmail.com>
2020-06-09 21:57:36 +0300

    lesson03

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-const money = 10,
+'use strict';
+
+let money = 10000,
     income = 'муж',
     addExpenses = 'машина,Дача,Шопинг',
     deposit = true,
@@ -13,5 +15,32 @@ console.log(`Период равен ${period} месяцев`, `Цель зар
 
 console.log(addExpenses.toLowerCase().split(','));
 
-const budgetDay = money / 30;
+let budgetDay = money / 30;
 console.log(budgetDay);
+
+// lesson03
+money = prompt('Ваш месячный доход?');
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+deposit = confirm('Есть ли у вас депозит в банке?');
+
+const expenses1 = prompt('Введите обязательную статью расходов?');
+const amount1 = prompt('Во сколько это обойдется?');
+const expenses2 = prompt('Введите обязательную статью расходов?');
+const amount2 = prompt('Во сколько это обойдется?');
+
+const budgetMonth = money - amount1 - amount2;
+console.log('budgetMonth', budgetMonth);
+
+console.log('month count to the mission: ', Math.ceil(mission / budgetMonth));
+budgetDay = Math.floor(budgetMonth / 30);
+console.log('budgetDay', budgetDay);
+
+if (budgetDay >= 1200) {
+    alert('У вас высокий уровень дохода');
+} else if (budgetDay >= 600 && budgetDay < 1200) {
+    alert('У вас средний уровень дохода');
+} else if (budgetDay < 600) {
+    alert('К сожалению у вас уровень дохода ниже среднего');
+} else if (budgetDay < 0) {
+    alert('Что то пошло не так');
+}
